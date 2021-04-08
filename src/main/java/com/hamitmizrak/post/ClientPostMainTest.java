988bced5a01b82fbda3fd6d46101badb7df68bca
HamitMizrak <hamitmizrak@gmail.com>
2021-04-08 13:52:07 +0300

    post manuel

---
+++ src/main/java/com/hamitmizrak/post/ClientPostMainTest.java
@@ -0,0 +1,23 @@
+package com.hamitmizrak.post;
+
+import javax.json.Json;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+
+public class ClientPostMainTest {
+
+	public static void main(String[] args) {
+
+		String json = "{\r\n" + "\"adi\":\"Hamit\",\r\n" + "\"soyadi\":\"Mızrak\"\r\n" + "}";
+		String json2 = Json.createObjectBuilder().add("adi", "Hamit44").add("soyadı", "Mızrak").build().toString();
+
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target("http://localhost:8080/JerseyRest2/jersey2/post/veriTuket");
+
+		webTarget.request("application/json; charset=UTF-8")
+				.post(Entity.entity(json2, "application/json; charset=UTF-8"), Void.class);
+	}
+
+}
---
+++ src/main/java/com/hamitmizrak/post/PostVeriTuket.java
@@ -0,0 +1,24 @@
+package com.hamitmizrak.post;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/post")
+public class PostVeriTuket {
+
+	// PVC
+	@POST
+	@Path("/veriManuel")
+	@Consumes("application/json; charset=UTF-8")
+	public void postMethod(String veriAl) {
+		System.out.println(veriAl);
+	}
+
+	@POST
+	@Path("/veriDynamics")
+	@Consumes("application/json; charsete=UTF-8")
+	public void setTelephone(Telephone telephone) {
+		System.out.println(telephone);
+	}
+}
